32f5baa332cb8e59d4ca4791f9efe0aa16070dd2
Martin Cribbins <M50505@stonefifteen.com>
2025-02-22 14:22:58 -0500

    First draft

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-module.exports = {
-  images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/my-account/**",
-      },
-    ],
-  },
-};
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {};
+
+export default config;
